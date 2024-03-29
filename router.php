7bbbf0c7f7e78a35153ea3f464b34c250859a4d2
kevino430 <ruby004949@gmail.com>
2024-03-29 17:51:11 +0800

    update router.php test 001 fix css js fails

--- router.php
+++ router.php
@@ -1,7 +1,13 @@
 <?php
 
-// 简单的路由：无论接收到什么请求，都执行 api_test.php
-include('index.php');
-// hello wolrd 
-// add main 
+$request = $_SERVER['REQUEST_URI'];
 
+// 检查请求是否指向静态文件
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $request)) {
+    // 对于静态文件，尝试直接返回这些文件
+    // 返回 false 以允许服务器的默认处理方式，不需要修改此行
+    return false;
+} else {
+    // 对于其他所有请求，包含 index.php
+    include('index.php');
+}
