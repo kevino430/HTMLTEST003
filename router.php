8bd0e2a775f4c1dfc8fe6778b2b721606760bdd5
kevino430 <ruby004949@gmail.com>
2024-04-01 11:32:16 +0800

    fix icons

--- router.php
+++ router.php
@@ -3,11 +3,10 @@
 $request = $_SERVER['REQUEST_URI'];
 
 // 检查请求是否指向静态文件
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $request)) {
-    // 对于静态文件，尝试直接返回这些文件
-    // 返回 false 以允许服务器的默认处理方式，不需要修改此行
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js|eot|woff2|woff|ttf|svg)$/', $request)) {
+    // Serve the static files directly.
     return false;
 } else {
-    // 对于其他所有请求，包含 index.php
+    // Include 'index.php' for all other requests.
     include('index.php');
 }
