e379d5264feaf437c844da7515d90eb9619f2446
kevino430 <ruby004949@gmail.com>
2024-04-01 11:50:42 +0800

    fix ruoter.php

--- router.php
+++ router.php
@@ -1,12 +1,16 @@
 <?php
 
-$request = $_SERVER['REQUEST_URI'];
+// $request = $_SERVER['REQUEST_URI'];
 
-// 检查请求是否指向静态文件
-if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js|eot|woff2|woff|ttf|svg)$/', $request)) {
-    // Serve the static files directly.
-    return false;
-} else {
-    // Include 'index.php' for all other requests.
-    include('index.php');
-}
+// // 检查请求是否指向静态文件
+// if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js|eot|woff2|woff|ttf|svg)$/', $request)) {
+//     // Serve the static files directly.
+//     return false;
+// } else {
+//     // Include 'index.php' for all other requests.
+//     include('index.php');
+// }
+
+
+// 对所有文件路径返回 false，让内建服务器自己处理。
+return false;
